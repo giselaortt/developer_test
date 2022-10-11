b6d93fdf7dcbfad5706790e4dd574449cc8ec203
gisela ortt <ms.giselaortt@gmail.com>
2022-10-11 14:23:39 -0300

    dunder str

--- take5/survey/models.py
+++ take5/survey/models.py
@@ -16,6 +16,7 @@ class Survey( models.Model ):
 	id_question_8 = models.IntegerField()
 	id_question_9 = models.IntegerField()
 	id_question_10 = models.IntegerField()
+    def __str__(self):
 
 
 class SurveyQuestion( models.Model ):
@@ -31,6 +32,10 @@ class SurveyQuestion( models.Model ):
 	alternative_id_4 = models.IntegerField()
 	alternative_id_5 = models.IntegerField()
 
+    def __str__(self):
+
+        return self.question_text
+
 
 class SurveyQuestionAlternative( models.Model ):
 	LENGTH_ALTERNATIVE = 100
@@ -38,8 +43,14 @@ class SurveyQuestionAlternative( models.Model ):
 	alternative_text = models.CharField(max_length=LENGTH_ALTERNATIVE)	
 	alternative_id = models.IntegerField()
 
+    def __str__(self):
+
+        return self.alternative_text
+
 
 class SurveyUserAnswer( models.Model ):
+    MAX_NAME_LENGTH = 50
+    user_name = models.CharField( MAX_NAME_LENGTH )
 	user_answer = models.IntegerField()
 	survey_id = models.IntegerField()
 	user_answer_question_1 = models.CharField( max_length = 1 )
@@ -53,3 +64,5 @@ class SurveyUserAnswer( models.Model ):
 	user_answer_question_9 = models.CharField( max_length = 1 )
 	user_answer_question_10 = models.CharField( max_length = 1 )
 
+    def __str__(self):
+        pass
