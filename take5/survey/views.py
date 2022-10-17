dd01cc761a2c87d17ae23be8a0aaaa74e5e8da08
gisela ortt <ms.giselaortt@gmail.com>
2022-10-17 17:09:34 -0300

    FIX: many=True

--- take5/survey/views.py
+++ take5/survey/views.py
@@ -1,30 +1,40 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
 from .models import SurveyQuestionAlternative, SurveyQuestion, Survey, SurveyUserAnswer
+from .serializers import *
 
 
 def test( request ):
+
     return HttpResponse('hallo world welcome to my page')
 
 
 def index( request ) :
+
     return HttpResponse('imagine here a creative wonderful index test')
 
 
-class SurveyQuestionAlternativeView(): 
+def SurveyView( request ):
+    survey = Survey.objects.all()
+    questions = SurveyQuestion.objects.all()
+    alternatives = SurveyQuestionAlternative.objects.all()
+    serializer = SurveySerializer( survey, many= True )
+    print(serializer.data)
+
+    return HttpResponse(serializer.data)
+
+
+'''
+class SurveyQuestionAlternativeView():
     pass
 
 
-class SurveyQuestionView(): 
+class SurveyQuestionView():
     pass
 
 
-class SurveyView(): 
+
+class SurveyUserAnswerView():
     pass
-
-
-class SurveyUserAnswerView(): 
-    pass
-
+'''
 
